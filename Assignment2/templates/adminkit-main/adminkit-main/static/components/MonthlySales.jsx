a8321a816be3e58239c1edd2ad24502c67fb3261
Maheen Fatime <bscs22093@itu.edu.pk>
2024-10-07 20:37:51 +0500

    make a component for monthly sales record  in admin

--- Assignment2/templates/adminkit-main/adminkit-main/static/components/MonthlySales.jsx
+++ Assignment2/templates/adminkit-main/adminkit-main/static/components/MonthlySales.jsx
@@ -5,7 +5,7 @@ const MonthlySales = () => {
         <div className="col-12 col-lg-4 col-xxl-3 d-flex">
             <div className="card flex-fill w-100">
                 <div className="card-header">
-                    <h5 className="card-title mb-0">Monthly Sales</h5>
+                    <h5 className="card-title mb-0">Monthly Saless</h5>
                 </div>
                 <div className="card-body d-flex w-100">
                     <div className="align-self-center chart chart-lg">
