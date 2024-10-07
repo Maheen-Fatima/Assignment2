88110d76cb6ffd2ca117d1c46c8d49a778a24a09
Maheen Fatime <bscs22093@itu.edu.pk>
2024-10-07 20:08:34 +0500

    initial push

---
+++ Assignment2/templates/adminkit-main/adminkit-main/static/components/LatestProjects.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const LatestProjects = () => {
+    return (
+        <div className="col-12 col-lg-8 col-xxl-9 d-flex">
+            <div className="card flex-fill">
+                <div className="card-header">
+                    <h5 className="card-title mb-0">Latest Projects</h5>
+                </div>
+                <table className="table table-hover my-0">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th className="d-none d-xl-table-cell">Start Date</th>
+                            <th className="d-none d-xl-table-cell">End Date</th>
+                            <th>Status</th>
+                            <th className="d-none d-md-table-cell">Assignee</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* Sample Data */}
+                        <tr>
+                            <td>Project Apollo</td>
+                            <td className="d-none d-xl-table-cell">01/01/2023</td>
+                            <td className="d-none d-xl-table-cell">31/06/2023</td>
+                            <td><span className="badge bg-success">Done</span></td>
+                            <td className="d-none d-md-table-cell">Vanessa Tucker</td>
+                        </tr>
+                        <tr>
+                            <td>Project Fireball</td>
+                            <td className="d-none d-xl-table-cell">01/01/2023</td>
+                            <td className="d-none d-xl-table-cell">31/06/2023</td>
+                            <td><span className="badge bg-danger">Cancelled</span></td>
+                            <td className="d-none d-md-table-cell">William Harris</td>
+                        </tr>
+                        {/* More projects can be added here */}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default LatestProjects;
---
+++ Assignment2/templates/adminkit-main/adminkit-main/static/components/MonthlySales.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MonthlySales = () => {
+    return (
+        <div className="col-12 col-lg-4 col-xxl-3 d-flex">
+            <div className="card flex-fill w-100">
+                <div className="card-header">
+                    <h5 className="card-title mb-0">Monthly Sales</h5>
+                </div>
+                <div className="card-body d-flex w-100">
+                    <div className="align-self-center chart chart-lg">
+                        <canvas id="chartjs-dashboard-bar"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MonthlySales;
